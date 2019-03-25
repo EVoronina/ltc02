46d32632e1f359b65b95cbb0e07862b756884050
U-MAIN\smolyakova <smolyakova@No121.main.a-3.ru>
2019-03-25 10:17:09 +0300

    check palindrome

--- js/palindrome2.js
+++ js/palindrome2.js
@@ -1,11 +1,17 @@
 function isPalindrome(text) {
-    var str = text.toLocaleLowerCase().split(/[^a-z]*/).join("");
-    var strReverse = text.toLowerCase().split(/[^a-z]*/).reverse().join("");
-
-if(str==strReverse) {
-    return true
-} else {
-    return false
+    var str = text.toLowerCase().split(/[^a-z]*/).join("");
+//     console.log(str);
+    if(str.length >= 2){
+//         console.log("str >=2");
+        if(str[0]==str[str.length - 1]) {    
+            return isPalindrome(str.substr(1,str.length - 2));
+        } else {
+            return false;
+        }
+    }else{
+        return true;
+    }
 }
-}
-console.log(isPalindrome("Was it a cat I saw?"));
+console.log(isPalindrome("Not New York, Roy went on"));
+console.log(isPalindrome("A man, a plan a canal-Panama"));
+console.log(isPalindrome("dfgdsfgsdfgs"));
