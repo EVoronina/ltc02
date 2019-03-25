e8ac9d4dbc6b4bff8c913507c48052c706a324bf
U-MAIN\smolyakova <smolyakova@No121.main.a-3.ru>
2019-03-25 10:18:33 +0300

    check palindrome

--- js/palindrome2.js
+++ js/palindrome2.js
@@ -1,8 +1,7 @@
 function isPalindrome(text) {
     var str = text.toLowerCase().split(/[^a-z]*/).join("");
-//     console.log(str);
     if(str.length >= 2){
-//         console.log("str >=2");
+
         if(str[0]==str[str.length - 1]) {    
             return isPalindrome(str.substr(1,str.length - 2));
         } else {
